cb9a5b282f0c5b70dcca49e4d0430c7713eac7e5
Carol Greene <carolgreene3@gmail.com>
2018-11-24 23:32:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+document.addEventListener('DOMContentLoaded',
+function(event) {
+  Handlebars.registerPartial(
+    'authorPartial',
+    document.getElementById('author-partial-template').innerHTML
+  );
+});
+
+
 function getRepositories() {
   const req = new XMLHttpRequest();
   req.addEventListener('load', showRepositories);
@@ -13,11 +22,3 @@ function showRepositories(event, data) {
 
 document.getElementById('repositories').innerHTML = repoList;
 }
-
-document.addEventListener('DOMContentLoaded',
-function(event) {
-  Handlebars.registerPartial(
-    'authorPartial',
-    document.getElementById('author-partial-template').innerHTML
-  );
-});
